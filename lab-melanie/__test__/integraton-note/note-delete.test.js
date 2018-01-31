fad675ee39d88c1eac841a9fd2dedb239e459a2d
Melanie Downing <sayanything830@gmail.com>
2018-01-30 16:56:33 -0800

    finished delete tests

--- lab-melanie/__test__/integraton-note/note-delete.test.js
+++ lab-melanie/__test__/integraton-note/note-delete.test.js
@@ -31,7 +31,24 @@ describe('DELETE', function() {
       expect(this.response.title).toBe(undefined);
     });
     it('should no longer have an id', () => {
-      expect(this.ressponse.body._id).toBe(undefined);
+      expect(this.response._id).toBe(undefined);
+    });
+  });
+
+  describe('Invalid req/res', () => {
+    it('should return a status code 404 with a bad schema', () => {
+      return superagent.delete(':4000/api/v1/doesnotexist')
+        .send()
+        .catch(err => {
+          expect(err.status).toBe(404);
+        });
+    });
+    it('should return a status code 404 with a bad request', () => {
+      return superagent.delete(':4000/api/v1/note')
+        .send({title: '', content: ''})
+        .catch(err => {
+          expect(err.status).toBe(404);
+        });
     });
   });
 });

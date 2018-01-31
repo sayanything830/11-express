4513e5f3d03b8f0df0a7afc4fe1495f3574cedaf
Melanie Downing <sayanything830@gmail.com>
2018-01-30 21:22:17 -0800

    note test and integration tests done

--- lab-melanie/__test__/unit-tests/error-handler.test.js
+++ lab-melanie/__test__/unit-tests/error-handler.test.js
@@ -1,7 +1,14 @@
 'use strict';
 
+// const errorHandler = require('../../lib/error-handler.js');
 require('jest');
 
-describe('testing dummy', function() {
-  it('should return true', () => expect(true).toBeTruthy());
+let errOne = new Error('validation error');
+errOne.res = null;
+
+
+describe('Error Handler', function() {
+  it('should return a 400 status for a validation error', () => {
+    expect(true).toBe(true); // need to work on this
+  });
 });
--- lab-melanie/__test__/unit-tests/storage.test.js
+++ lab-melanie/__test__/unit-tests/storage.test.js
@@ -1,7 +1,32 @@
 'use strict';
 
+// let storage = require('../../lib/storage');
 require('jest');
 
-describe('testing dummy', function() {
-  it('should return true', () => expect(true).toBeTruthy());
+describe('Storage Module', function() {
+  describe('#create', () => {
+    it('should return true', () => {
+      expect(true).toBe(true); // setting up dummy tests
+    });
+  });
+  describe('#fetchOne', () => {
+    it('should return true', () => {
+      expect(true).toBe(true); // setting up dummy tests
+    });
+  });
+  describe('#fetchAll', () => {
+    it('should return true', () => {
+      expect(true).toBe(true); // setting up dummy tests
+    });
+  });
+  describe('#update', () => {
+    it('should return true', () => {
+      expect(true).toBe(true); // setting up dummy tests
+    });
+  });
+  describe('#delete', () => {
+    it('should return true', () => {
+      expect(true).toBe(true); // setting up dummy tests
+    });
+  });
 });

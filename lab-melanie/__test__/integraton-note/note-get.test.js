79248d7d77f2f5d0e1a99c7dda0f6209b39a7527
Melanie Downing <sayanything830@gmail.com>
2018-01-30 16:11:35 -0800

    post and get tests passing

--- lab-melanie/__test__/integraton-note/note-get.test.js
+++ lab-melanie/__test__/integraton-note/note-get.test.js
@@ -1,53 +1,60 @@
 'use strict';
 
+const server = require('../../lib/server.js');
+const superagent = require('superagent');
 require('jest');
 
-describe('testing dummy', function() {
-  it('should return true', () => expect(true).toBeTruthy());
+describe('GET', function() {
+  this.mockOne = {title: 'hello', content: 'hello world'};
+  this.mockTwo = {title: 'two', content: 'hello again'};
+  beforeAll(() => server.start(process.env.PORT, () => console.log(`Listening on ${process.env.PORT}`)));
+  afterAll(() => server.stop());
+
+  describe('Valid req/res', () => {
+    beforeAll(() => {
+      return superagent.post(':4000/api/v1/note')
+        .send(this.mockOne)
+        .then(res => {
+          this.resOne = res;
+          return superagent.post(':4000/api/v1/note')
+            .send(this.mockTwo)
+            .then(res => this.resTwo = res);
+        });
+    });
+
+    beforeAll(() => {
+      return superagent.get(':4000/api/v1/note')
+        .then(res => this.getOne = res);
+    });
+    
+    it('should return an array of ids', () => {
+      this.getOne.body.map(id => {
+        expect(id).toMatch(/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/);
+      });
+    });
+    it('should return a status code 200', () => {
+      expect(this.getOne.status).toBe(200);
+    });
+    it('should contain the two ids of records posted', () => {
+      expect(this.getOne.body).toContain(this.resOne.body._id);
+      expect(this.getOne.body).toContain(this.resTwo.body._id);
+    });
+  });
+
+  describe('Invalid req/res', () => {
+    it('should return a status code 400 without schema', () => {
+      return superagent.get(':4000/api/v1/note')
+        .send()
+        .catch(err => {
+          expect(err.status).toBe(400);
+        });
+    });
+    it('should return a 404 given an incorrect path', () => {
+      return superagent.get(':4000/api/v1/doesnotexist')
+        .send({title: '', content: ''})
+        .catch(err => {
+          expect(err.status).toBe(404);
+        });
+    });
+  });
 });
-
-// 'use strict';
-
-// const server = require('../../lib/server.js');
-// const superagent = require('superagent');
-// require('jest');
-
-// describe('GET', function() {
-//     beforeAll(() => server.start(process.env.PORT, () => console.log(`Listening on ${process.env.PORT}`)));
-
-//   afterAll(() => server.stop());
-
-//   describe('GET /api/v1/note', () => {
-//     let resOne, resTwo, getOne;
-
-//     beforeAll(() => {
-//       return superagent.post(':4002/api/v1/note')
-//         .send({title: 'hello', content: 'yo'})
-//         .then(res => {
-//           resOne = res;
-//           return superagent.post(':4002/api/v1/note')
-//             .send({title: 'bye', content: 'yup'})
-//             .then(res => {
-//               resTwo = res;
-//             });
-//         });
-//     });
-//     beforeAll(() => {
-//       return superagent.get(':4002/api/v1/note')
-//         .then(res => getOne = res);
-//     });
-
-//     it('should return an array of ids', () => {
-//       getOne.body.map(id => {
-//         expect(id).toMatch(/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/);
-//       });
-//     });
-//     it('should return a status code 200', () => {
-//       expect(getOne.status).toBe(200);
-//     });
-//     it('should contain the two ids of records posted', () => {
-//       expect(getOne.body).toContain(resOne.body._id);
-//       expect(getOne.body).toContain(resTwo.body._id);
-//     });
-//   });
-// });
